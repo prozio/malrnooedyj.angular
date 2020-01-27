4e06ff1e040552ea161cd78c6a3603ae268ca996
a67z <luca.onorati@gft.com>
2020-01-27 17:44:02 +0100

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,9 +30,3 @@ import { CartComponent } from './cart/cart-component';
 })
 export class AppModule { }
 
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
--- src/app/series.ts
+++ src/app/series.ts
@@ -1,18 +1,18 @@
 export const series = [
   {
-    id:12,
+    id:1,
     name: 'Orange County',
     price: 39.9,
     description: '4 seasons'
   },
   {
-    id:16,
+    id:2,
     name: 'Dawson s Creek',
     price: 59.9,
     description: '6 seasons'
   },
   {
-    id:33,
+    id:3,
     name: 'Friends',
     price: 99.9,
     description: '10 seasons, best sitcom ever'
